ce42cd58d8347dc246b2c0fe5a28edf1f533b026
Farukuzzaman,Sheikh <sfsohel1895@gmail.com>
2022-08-22 20:10:07 +0600

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import axios from "axios";
+import DatetimePicker from 'vuetify-datetime-picker'
+Vue.use(DatetimePicker)
+
 Vue.config.productionTip = false
 if (localStorage.getItem('user-token')) {
   axios.defaults.headers.common = { 'Authorization': `Bearer ${localStorage.getItem('user-token')}` }
--- src/views/Backend-master/projects/AddProjects.vue
+++ src/views/Backend-master/projects/AddProjects.vue
@@ -16,41 +16,109 @@
                                                 <v-text-field v-model="name" label="Project Name"></v-text-field>
                                             </v-col>
                                             <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="Project ID"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="Home Page"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="Status"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="Type"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="Attachment"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="Client"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="Project Cost"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="Start Date"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="End Date"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field v-model="name" label="End Date"></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" sm="6" md="4">
-                                                <v-text-field label="Time Revised" v-model="name" suffix="Days">
+                                                <v-text-field v-model="project_code" label="Project Code">
                                                 </v-text-field>
                                             </v-col>
                                             <v-col cols="12" sm="6" md="4">
-                                                <v-text-field label="Cost Revised" v-model="name" suffix="Amount">
+                                                <v-text-field v-model="home_page" label="Home Page"></v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-select :items="['On Going','Finished']" label="Standard"
+                                                    v-model="status"></v-select>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-select :items="['General','Consultency']" label="Type"
+                                                    v-model="type"></v-select>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-file-input chips multiple show-size truncate-length="24"
+                                                    v-model="attachment">
+                                                </v-file-input>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="client" label="Client"></v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="project_cost" label="Project Cost">
+                                                </v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="contact_person" label="Contact Person">
+                                                </v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <!-- <v-datetime-picker label="Select Datetime" v-model="start_date"></v-datetime-picker> -->
+                                                <v-menu v-model="start_date_menu" :close-on-content-click="false"
+                                                    :nudge-right="40" transition="scale-transition" offset-y
+                                                    min-width="auto">
+                                                    <template v-slot:activator="{ on, attrs }">
+                                                        <v-text-field v-model="start_date" label="Start Date"
+                                                            prepend-icon="mdi-calendar" readonly v-bind="attrs"
+                                                            v-on="on"></v-text-field>
+                                                    </template>
+                                                    <v-date-picker v-model="start_date"
+                                                        @input="start_date_menu = false">
+                                                    </v-date-picker>
+                                                </v-menu>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <!-- <v-text-field v-model="awarded_date" label="Awarded Date"></v-text-field> -->
+                                                <v-menu v-model="awarded_date_menu" :close-on-content-click="false"
+                                                    :nudge-right="40" transition="scale-transition" offset-y
+                                                    min-width="auto">
+                                                    <template v-slot:activator="{ on, attrs }">
+                                                        <v-text-field v-model="awarded_date" label="Awarded Date"
+                                                            prepend-icon="mdi-calendar" readonly v-bind="attrs"
+                                                            v-on="on"></v-text-field>
+                                                    </template>
+                                                    <v-date-picker v-model="awarded_date"
+                                                        @input="awarded_date_menu = false">
+                                                    </v-date-picker>
+                                                </v-menu>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <!-- <v-text-field v-model="end_date" label="End Date"></v-text-field> -->
+                                                <v-menu v-model="end_date_menu" :close-on-content-click="false"
+                                                    :nudge-right="40" transition="scale-transition" offset-y
+                                                    min-width="auto">
+                                                    <template v-slot:activator="{ on, attrs }">
+                                                        <v-text-field v-model="end_date" label="End Date"
+                                                            prepend-icon="mdi-calendar" readonly v-bind="attrs"
+                                                            v-on="on"></v-text-field>
+                                                    </template>
+                                                    <v-date-picker v-model="end_date" @input="end_date_menu = false">
+                                                    </v-date-picker>
+                                                </v-menu>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-select :items="['Government','Non Government']" label="Standard"
+                                                    v-model="funded_by"></v-select>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field label="Time Revised" v-model="time_revised" suffix="Days">
+                                                </v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="cost_revised" label="Cost Revised"
+                                                    suffix="Amount">
+                                                </v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="description" label="Description"></v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="objective" label="Objective"></v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="reference_terms" label="Reference Terms">
+                                                </v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="technical_document"
+                                                    label="Tecnical Documentation"></v-text-field>
+                                            </v-col>
+                                            <v-col cols="12" sm="6" md="4">
+                                                <v-text-field v-model="solution_component" label="Solution Component">
                                                 </v-text-field>
                                             </v-col>
                                         </v-row>
@@ -65,9 +133,32 @@
     </v-layout>
 </template>
 <script>
+import axios from 'axios'
 export default {
     data: () => ({
         name:'',
+        project_code:'',
+        home_page:'',
+        status:'',
+        type:'',
+        attachment:null,
+        client:'',
+        project_cost:'',
+        contact_person:'',
+        start_date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        start_date_menu: false,
+        awarded_date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        awarded_date_menu:false,
+        end_date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        end_date_menu:false,
+        funded_by:'',
+        time_revised:'',
+        cost_revised:'',
+        description:'',
+        objective:'',
+        reference_terms:'',
+        technical_document:'',
+        solution_component:'',
     }),
 
     computed: {
@@ -83,7 +174,39 @@ export default {
     },
 
     methods: {
-        
+        async saveDetais() {
+            let formData = new FormData();
+            this.attachment.forEach(element => {
+                formData.append('attachment[]', element);
+            });
+            formData.append('name', this.name);
+            formData.append('project_code', this.project_code);
+            formData.append('home_page', this.home_page);
+            formData.append('status', this.status);
+            formData.append('type', this.type);
+            formData.append('client', this.client);
+            formData.append('project_cost', this.project_cost);
+            formData.append('contact_person', this.contact_person);
+            formData.append('start_date', this.start_date);
+            formData.append('awarded_date', this.awarded_date);
+            formData.append('end_date', this.end_date);
+            formData.append('funded_by', this.funded_by);
+            formData.append('time_revised', this.time_revised);
+            formData.append('cost_revised', this.cost_revised);
+            formData.append('description', this.description);
+            formData.append('objective', this.objective);
+            formData.append('reference_terms', this.reference_terms);
+            formData.append('technical_document', this.technical_document);
+            formData.append('solution_component', this.solution_component);
+            let result = await axios.post(`project`, formData);
+            if (result.data) {
+                console.log('good');
+                // this.cashoutSaveSuccess = true;
+                // this.file = [];
+                // this.dialog = false;
+                // this.$emit('getCallInfo');
+            }
+        }
     },
 }
 </script>
